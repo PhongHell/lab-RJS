62792497159a9c0cc2d1d128e5d2a3fb0a8e635b
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-10-04 11:02:02 +0700

    fetch form server

---
+++ src/redux/leaders.js
@@ -0,0 +1,8 @@
+import { LEADERS } from '../shared/leaders';
+//reducer leaders
+export const Leaders = (state=LEADERS,action) =>{
+    switch(action.type){
+        default : 
+         return state;
+    }
+}
