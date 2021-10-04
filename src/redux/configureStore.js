b6d272cabd51bd9fe748183bee7813ca778f96ce
Thanh Phong <daothanhphonga1k5@gmail.com>
2021-10-04 10:51:18 +0700

    combine reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,22 @@
+import {createStore , combineReducers} from 'redux';
+import {Comments} from './comments';
+import {Leaders} from './leaders';
+import {Promotions} from './promotions';
+import {Dishes} from './dishes';
+
+
+
+export const ConfigureStore = () =>{
+    const store = createStore (
+        combineReducers(
+            {
+                dishes: Dishes,
+                comments:Comments,
+                leaders:Leaders,
+                promotions: Promotions
+            }
+        )
+    );
+    return store;
+
+}
